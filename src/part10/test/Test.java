e90ae793ee7f6cc2450ef54b6e0ee4df953c0415
alex-bulgakov <master8423@gmail.com>
2016-11-24 11:11:30 +0300

    done part10

---
+++ src/part10/test/Test.java
@@ -0,0 +1,11 @@
+package part10.test;
+
+/**
+ * Created by alebul on 24.11.16.
+ */
+public class Test {
+    public static void main(String[] args) {
+        String s = "123.33";
+        System.out.println(Double.parseDouble(s));
+    }
+}
